c50eb3ce743041ada47ea31ce179e1cc75c24388
Antonio Gasco <antoniofgasco@gmail.com>
2021-01-27 10:47:34 +0000

    fix-in-query

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -12,21 +12,26 @@ function Games() {
   const [curGenres, setCurGenres] = useState(
     Array.from({ length: 19 }, (_, i) => i + 1)
   );
-  const [curMetacritic, setCurMetacritic] = useState([0, 100]);
+  const [curMetacritic, setCurMetacritic] = useState([10, 100]);
   const [curReleaseDate, setCurReleaseDate] = useState("1970-01-01,2021-12-31");
 
   useEffect(() => {
-    fetchGamesData();
+    //Building query
+    let query = `/games?page_size=40`;
+    if (searchQuery != "")
+      query = query.concat(`&search_exact=true${searchQuery}`);
+    if (curReleaseDate != "1970-01-01,2021-12-31")
+      query = query.concat(`&dates=${curReleaseDate}`);
+    query = query.concat(`&metacritic=${curMetacritic[0]},${curMetacritic[1]}`);
+    query = query.concat(
+      `&parent_platforms=${curPlatforms}&genres=${curGenres}&ordering=-metacritic`
+    );
+    console.log("query", query);
+
+    fetchGamesData(query);
   }, [searchQuery, curPlatforms, curGenres, curMetacritic, curReleaseDate]);
 
-  const fetchGamesData = async () => {
-    const query = `/games?page_size=40
-      &search_exact=true${searchQuery}
-      &parent_platforms=${curPlatforms}
-      &genres=${curGenres}
-      &metacritic=${curMetacritic[0]},${curMetacritic[1]}
-      &dates=${curReleaseDate}`;
-    console.log("query", query);
+  const fetchGamesData = async (query) => {
     await axios
       .get(query)
       .then((res) => {
@@ -66,9 +71,9 @@ function Games() {
   const handleMetacriticChange = (e, newValue) => {
     //default
     console.log("newValue", newValue);
-    if (newValue.includes(10) && newValue.includes(100))
-      setCurMetacritic([0, 100]);
-    else setCurMetacritic(newValue);
+    // if (newValue.includes(10) && newValue.includes(100))
+    //   setCurMetacritic([0, 100]);
+    setCurMetacritic(newValue);
   };
 
   const handleReleaseDateChange = (newValue) => {
