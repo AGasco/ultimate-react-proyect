e8bf1a0511f4be228ff30a6df64c18eaccef533e
Antonio Gasco <antoniofgasco@gmail.com>
2021-03-23 09:06:33 +0000

    fix-safety

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 const axios = require("axios").default;
 const endpoint = "https://rawg-video-games-database.p.rapidapi.com";
-export const API_KEY = "367a3539cbb94792bb4ecaef0601b21c";
+export const API_KEY = process.env.REACT_APP_RAWG_API_KEY;
 
 const options = {
   baseURL: endpoint,
@@ -8,7 +8,7 @@ const options = {
     "User-Agent": "The-Ultimate-Games-Display/0.1",
     "Content-type": "application/json",
     token: API_KEY,
-    "x-rapidapi-key": "87594fe772msh55e0b060c7b1ac1p1b869bjsn1a9c545a181d",
+    "x-rapidapi-key": process.env.REACT_APP_RAWG_RAPIDAPI_KEY,
     "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
   },
 };
